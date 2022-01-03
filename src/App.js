4a3c546798405561843650004844b8b866f904e0
hrishikesh <hshinde166@gmail.com>
2022-01-04 01:24:10 +0530

    Certain components have been added and certain changes have been made

--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
 
-//Firebase
-import { initializeApp } from 'firebase/app';
-import "firebase/auth";
-
 //Components
 import Home from "./Pages/Home";
 import Signin from "./Pages/Signin";
@@ -17,6 +13,16 @@ import PageNotFound from "./Pages/PageNotFound";
 import userContext from './Context/UserContext';
 import Footer from "./Layout/Footer";
 import NavBar from "./Layout/NavBar";
+
+//Firebase
+import { initializeApp } from 'firebase/app';
+import "firebase/auth";
+
+//Firebase file
+import firebaseConfig from "./Config/FirebaseConfig";
+
+//Init Firebase
+const app = initializeApp(firebaseConfig); //We successfully integrated the firebase in our application. It is very important to initialize it in the main file before any other component is rendered
 
 function App() {
 
@@ -30,7 +36,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/signin" element={<Signin />} />
-          <Route path="/Signup" element={<Signup />} />
+          <Route path="/signup" element={<Signup />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
         <Footer />
