aacf5ec0f400397a72b383c9b14d10180b271820
hrishikesh <hshinde166@gmail.com>
2022-01-03 23:14:05 +0530

    Created the footer component here

---
+++ src/Layout/Footer.js
@@ -0,0 +1,11 @@
+import {Container} from "reactstrap";
+
+const Footer = () => {
+    return(
+        <Container fluid tag="footer" className="text-center bg-dark text-white p-3 text-uppercase fixed-bottom">
+            Hrishi Github-Finder
+        </Container>
+    );
+}
+
+export default Footer;
