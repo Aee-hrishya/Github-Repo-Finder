8296928796d1ccaffd4964054cf298d4882e12c6
hrishikesh <hshinde166@gmail.com>
2022-01-03 23:44:05 +0530

    Created the Firebase config file which is needed for the project

---
+++ src/Config/FirebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyADWIDXwSJ8aHDWiZWDmeKnElVtjaF-zfc",
+    authDomain: "github-finder-f20db.firebaseapp.com",
+    projectId: "github-finder-f20db",
+    storageBucket: "github-finder-f20db.appspot.com",
+    messagingSenderId: "122194654082",
+    appId: "1:122194654082:web:a378300c8e051418ab559b",
+    measurementId: "G-S5WLSQ85DT"
+  };
+
+  export default firebaseConfig;
